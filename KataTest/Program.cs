3f5885b6ba703481153b3b50d818bf5384d2fced
Robert Dannelly <robertdannelly@gmail.com>
2020-03-19 21:38:08 -0400

    Initial commit

---
+++ KataTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace KataDojo
+{
+
+    public class Kata
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static int CountBits(int n)
+        {
+            string binary = Convert.ToString(n, 2);
+            int countOnes = binary.Count(x => x == '1');
+            return countOnes;
+        }
+    }
+}
---
+++ NUnitKataTest/KataTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using KataDojo;
+
+[TestFixture]
+public class KataTest
+{
+    [TestFixture]
+    public class BitCounting
+    {
+        [Test]
+        public void CountBits()
+        {
+            Assert.AreEqual(0, Kata.CountBits(0));
+            Assert.AreEqual(1, Kata.CountBits(4));
+            Assert.AreEqual(3, Kata.CountBits(7));
+            Assert.AreEqual(2, Kata.CountBits(9));
+            Assert.AreEqual(2, Kata.CountBits(10));
+        }
+    }
+}
